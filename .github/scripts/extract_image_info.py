418f4a149d3ddb077ef3dee0a8f8c1ca231c9a04
sholdee <sholdee@gmail.com>
2024-08-07 09:59:17 -0500

    process mongo image updates

--- .github/scripts/extract_image_info.py
+++ .github/scripts/extract_image_info.py
@@ -22,20 +22,33 @@ def main():
         if file['filename'].endswith('.yaml') or file['filename'].endswith('.yml'):
             patch = file['patch']
             lines = patch.split('\n')
-            for line in lines:
-                match = re.match(r'^\+ *(?:image|[a-z_]+image|imageName): *(.+)', line, re.IGNORECASE)
-                if match:
-                    image_tag = match.group(1).strip()
-                    
-                    image = image_tag.split('@')[0].split(':')[0].strip()
-                    tag = image_tag.split(':')[1].split('@')[0].strip() if ':' in image_tag else ""
-                    digest = image_tag.split('@')[1].strip() if '@' in image_tag else ""
             
-                    with open(os.environ['GITHUB_ENV'], 'a') as f:
-                        f.write(f"IMAGE={image}\n")
-                        f.write(f"TAG={tag}\n")
-                        f.write(f"DIGEST={digest}\n")
-                    break  # Exit after finding the new image line
+            if re.search(r'^apps/.*/unifi-db.yml$', file['filename']):
+                for line in lines:
+                    match = re.match(r'^\+ *version: *"(.+)"', line, re.IGNORECASE)
+                    if match:
+                        version = match.group(1).strip()
+                        
+                        with open(os.environ['GITHUB_ENV'], 'a') as f:
+                            f.write(f"IMAGE=mongo\n")
+                            f.write(f"TAG={version}\n")
+                            f.write(f"DIGEST=\n")
+                        break  # Exit after finding the new version line
+            else:
+                for line in lines:
+                    match = re.match(r'^\+ *(?:image|[a-z_]+image|imageName): *(.+)', line, re.IGNORECASE)
+                    if match:
+                        image_tag = match.group(1).strip()
+                        
+                        image = image_tag.split('@')[0].split(':')[0].strip()
+                        tag = image_tag.split(':')[1].split('@')[0].strip() if ':' in image_tag else ""
+                        digest = image_tag.split('@')[1].strip() if '@' in image_tag else ""
+                        
+                        with open(os.environ['GITHUB_ENV'], 'a') as f:
+                            f.write(f"IMAGE={image}\n")
+                            f.write(f"TAG={tag}\n")
+                            f.write(f"DIGEST={digest}\n")
+                        break  # Exit after finding the new image line
 
 if __name__ == "__main__":
     main()
