545802fd8fac0fd99b56fef082eb0ca26f9e1843
Ethan Shold <sholdee@gmail.com>
2026-06-03 10:45:54 -0500

    chore(validation): parallelize shellcheck

--- hack/validate/shellcheck.sh
+++ hack/validate/shellcheck.sh
@@ -12,4 +12,21 @@ validate_select_tracked_regular_files "$@"
 validate_keep_extensions sh bash bats
 validate_skip_if_empty shell
 
-shellcheck -x "${VALIDATE_FILES[@]}"
+shellcheck_jobs="${SHELLCHECK_JOBS:-}"
+if [[ -z "${shellcheck_jobs}" ]]; then
+  shellcheck_jobs="$(getconf _NPROCESSORS_ONLN 2>/dev/null || true)"
+fi
+if [[ -z "${shellcheck_jobs}" ]]; then
+  shellcheck_jobs="$(sysctl -n hw.ncpu 2>/dev/null || true)"
+fi
+if [[ ! "${shellcheck_jobs}" =~ ^[1-9][0-9]*$ ]]; then
+  shellcheck_jobs=4
+fi
+
+shellcheck_batch_size="${SHELLCHECK_BATCH_SIZE:-4}"
+if [[ ! "${shellcheck_batch_size}" =~ ^[1-9][0-9]*$ ]]; then
+  shellcheck_batch_size=4
+fi
+
+printf '%s\0' "${VALIDATE_FILES[@]}" |
+  xargs -0 -n "${shellcheck_batch_size}" -P "${shellcheck_jobs}" shellcheck -x
